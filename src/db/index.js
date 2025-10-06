dfb3c398e07ed589b65389598cc72c295a870a2b
Gulshan-heap <gulshank22347@gmail.com>
2025-10-07 01:49:15 +0530

    Database is connected

--- src/db/index.js
+++ src/db/index.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants";
+import { DB_NAME } from "../constants.js";
 
 const connectDB = async ()=>{
     try {
@@ -8,7 +8,7 @@ const connectDB = async ()=>{
         //checking what's inside connectionInstance
         console.log(connectionInstance)
 
-        console.log('\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}');
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
         //this .connection.host is a instance of object connectionInstance
 
     } catch (error) {
--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
-require('dotenv').config()
+// require('dotenv').config()
 
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
-import connectDB from "./db";
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+// import connectDB from "./db";
 
+// connectDB()
+/*  FIRST APPROACH
+import express from "express"
+const app=express()
+
+;(async ()=>{ //this semicolon is for cleaning purposes
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("ERR: ",error);
+            throw error
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.log("ERROR: ",error);
+        throw error
+    }
+})()
+*/ //one way
+
+//other way
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+dotenv.config({
+    path: './env'
+})
 
 connectDB()
-
-
-
-
 
 /*  FIRST APPROACH
 import express from "express"
